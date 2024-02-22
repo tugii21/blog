81f7c8260105f69b8e77eb455da41a8af5d6dc81
tugii21 bat <batsaikhantuguldur21@gmail.com>
2024-02-22 14:14:08 +0000

    added class meta

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
+
+    class Meta:
+        ordering = ["-created_on"]
+        def __str__(self):
+            return f"The title of this post is{self.title} | written by {self.author}"
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")

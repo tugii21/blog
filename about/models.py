17f89b0605edc37a9bd5e4654689d1ac0236053d
tugii21 bat <batsaikhantuguldur21@gmail.com>
2024-02-26 19:58:02 +0000

    earned to use the CloudinaryField in our Django models and applied template logic to display these images

--- about/models.py
+++ about/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
@@ -17,3 +19,6 @@ class CollaborateRequest(models.Model):
 
     def __str__(self):
         return f"Collaboration request from {self.name}"
+
+        
+

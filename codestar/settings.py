d93ade8318977ddcbc56a48543442ad24bd1d6b3
tugii21 bat <batsaikhantuguldur21@gmail.com>
2024-02-26 19:34:54 +0000

    enable serving image file

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-s$d=rxewc3(+va(#1%fpj79%oy^2)59e#o0x!%74y(r6@hi1-o
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-tugii21-blog-hcxmcf8z3tc.ws-eu108.gitpod.io',
 '.herokuapp.com'
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
